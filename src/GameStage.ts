bbb8a6f073c68233cc58123c2ba7f209ad4e4bca
ximenpo <ximenpo@jiandan.ren>
2015-04-22 12:02:13 +0800

    洗牌动画结束后，增加移动动画。

--- src/GameStage.ts
+++ src/GameStage.ts
@@ -1,24 +1,54 @@
-class GameStage extends simple.GameStage{
-    public constructor(root:egret.DisplayObjectContainer){
+class GameStage extends simple.GameStage {
+    private _pokerSprites: egret.SpriteSheet;
+    private _pokerFramesData: simple.BitmapFramesData;
+    private _twShuffle: egret.Tween;
+
+    public constructor(root: egret.DisplayObjectContainer) {
         super(root);
+        this.initResources();
     }
 
-    start(){
-      var sheet : egret.SpriteSheet = RES.getRes("shuffle-cards");
-      var data  = new simple.BitmapFramesData();
-      data.fps  = 10;
-      data.textures = new Array<egret.Texture>();
-      for(var i : number = 0; i < 12; ++i){
-          data.textures.push(sheet.getTexture("" + i))
-      }
-      var bitmap= new simple.BitmapAnimation(data);
-      this.gameLayer.addChild(bitmap);
-      bitmap.play(2);
-
-      bitmap.addEventListener(simple.BitmapAnimation.EVENT_COMPLETED, this.onShuffleEnd, this);
+    private initResources() {
+        this._pokerSprites = RES.getRes("shuffle-cards");
+        var data = new simple.BitmapFramesData();
+        data.fps = 10;
+        data.textures = new Array<egret.Texture>();
+        for (var i: number = 0; i < 12; ++i) {
+            data.textures.push(this._pokerSprites.getTexture("" + i))
+        }
+        this._pokerFramesData = data;
     }
 
-    private onShuffleEnd(event:egret.Event){
-        console.log("completed!");
+    run() {
+        this._shuffleCards();
+    }
+
+    private _imgShuffle: simple.BitmapAnimation;
+    private _shuffleCards() {
+        if (!this._imgShuffle) {
+            var img = new simple.BitmapAnimation(this._pokerFramesData);
+            this.gameLayer.addChild(img);
+            this._imgShuffle = img;
+            img.addEventListener(simple.BitmapAnimation.EVENT_COMPLETED, this.onShuffleEnd, this);
+        }
+
+        var img = this._imgShuffle;
+        img.anchorX = 0.5;
+        img.anchorY = 0.5;
+        img.x = this.stage.stageWidth / 2;
+        img.y = this.stage.stageHeight / 2;
+
+        var tw = egret.Tween.get(this._imgShuffle);
+        tw.call(img.play, img, [2]);
+        tw.pause();
+        tw.wait(500);
+        tw.to({ y: this.stage.stageHeight / 5 }, 1000);
+        this._twShuffle = tw;
+    }
+
+    private onShuffleEnd(event: egret.Event) {
+        if (this._twShuffle) {
+            this._twShuffle.setPaused(false);
+        }
     }
 }
--- src/simple/GameStage.ts
+++ src/simple/GameStage.ts
@@ -1,49 +1,56 @@
-module simple{
+module simple {
 
-export  class GameStage extends egret.EventDispatcher {
-    //游戏场景层，游戏场景相关内容可以放在这里面。
-    private _gameLayer: egret.DisplayObjectContainer;
-    //GUI的组件必须都在这个容器内部,UIStage会始终自动保持跟舞台一样大小。
-    private _guiLayer:  egret.gui.UIStage;
-    //游戏特效层，位于GUI层之上的效果都放在这里面。
-    private _effectsLayer: egret.DisplayObjectContainer;
+    export class GameStage extends egret.EventDispatcher {
+        private _stage: egret.Stage;
+        //游戏场景层，游戏场景相关内容可以放在这里面。
+        private _gameLayer: egret.DisplayObjectContainer;
+        //GUI的组件必须都在这个容器内部,UIStage会始终自动保持跟舞台一样大小。
+        private _guiLayer: egret.gui.UIStage;
+        //游戏特效层，位于GUI层之上的效果都放在这里面。
+        private _effectsLayer: egret.DisplayObjectContainer;
 
-    public  get gameLayer(){
-        return  this._gameLayer;
+        public get stage() {
+            return this._stage;
+        }
+
+        public get gameLayer() {
+            return this._gameLayer;
+        }
+
+        public get guiLayer() {
+            return this._guiLayer;
+        }
+
+        public get effectsLayer() {
+            return this._effectsLayer;
+        }
+
+        public static EVENT_GAME_STARTED = "";
+
+        /**
+         *  构造函数
+         */
+        public constructor(private _root: egret.DisplayObjectContainer) {
+            super();
+
+            this._stage = egret.MainContext.instance.stage;
+
+            this._gameLayer = new egret.DisplayObjectContainer();
+            this._root.addChild(this._gameLayer);
+
+            this._guiLayer = new egret.gui.UIStage();
+            this._root.addChild(this._guiLayer);
+
+            this._effectsLayer = new egret.DisplayObjectContainer();
+            this._root.addChild(this._effectsLayer);
+
+            egret.callLater(this.run, this);
+        }
+
+        // 游戏启动入口
+        run() {
+            // game start logic. overridable.
+        }
     }
-
-    public  get guiLayer(){
-        return  this._guiLayer;
-    }
-
-    public  get effectsLayer(){
-        return  this._effectsLayer;
-    }
-
-    public static EVENT_GAME_STARTED  = "";
-
-    /**
-     *  构造函数
-     */
-    public  constructor(private _root: egret.DisplayObjectContainer){
-        super();
-
-        this._gameLayer = new egret.DisplayObjectContainer();
-        this._root.addChild(this._gameLayer);
-
-        this._guiLayer = new egret.gui.UIStage();
-        this._root.addChild(this._guiLayer);
-
-        this._effectsLayer = new egret.DisplayObjectContainer();
-        this._root.addChild(this._effectsLayer);
-
-        egret.callLater(this.start, this);
-    }
-
-    // 游戏启动入口
-    start() {
-        // game start logic. overridable.
-    }
-}
 
 }
